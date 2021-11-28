b58fb7fc0e0fb50c2f571833043d4ebf77868584
SyedHassan Asheghi <Asheghi.sh@gmail.com>
2021-11-28 20:38:16 +0330

    Updates

---
+++ _site/assets/_error.page.51e09ed5.js
@@ -0,0 +1,1 @@
+import{a as o,o as t,b as e}from"./vendor.31e6b9ee.js";const n={key:0},_=e("h1",null,"404 Page Not Found",-1),c=e("p",null,"This page could not be found.",-1),r=[_,c],d={key:1},i=e("h1",null,"500 Internal Server Error",-1),a=e("p",null,"Something went wrong.",-1),l=[i,a],m={props:["is404"],setup(s){return(h,u)=>s.is404?(t(),o("div",n,r)):(t(),o("div",d,l))}};export{m as default};
